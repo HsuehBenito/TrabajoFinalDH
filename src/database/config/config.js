34cc1e725778b6d18a5440045d8701eb6cca3c3e
HsuehBenito <bhsueh94@gmail.com>
2022-03-01 21:19:12 -0300

    db config and sequilizer

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports= {
+  "development": {
+    "username": "messi",
+    "password": "messiel10",
+    "database": "database_development",
+    "host":  "mysql-messi.alwaysdata.net",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
