4418a1af57e3d67570d66dd00e9efae553ccfbdd
HsuehBenito <bhsueh94@gmail.com>
2022-04-19 18:52:05 -0300

    api

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -7,47 +7,66 @@ const controller = {
 
 api:  (req,res) => {
     db.productos
-   .findAll()
+   .findAll({include:["categorias","administrador"]})
    .then(productos => {
        return res.status(200).json({
-           total: productos.length,
-           data: productos,
+           count: productos.length,
+           data: productos.map(producto=>{
+               return{
+                id: producto.nombre,
+                administrador: {nombre: producto.administrador.nombre_completo},
+                nombre: producto.nombre,
+                blend: producto.blend,
+                cosecha: producto.cosecha,
+                precio:  producto.precio,              
+                volumen: producto.volumen,
+                descripcion: producto.descripcion,
+                stock: producto.stock,
+                categoria: {nombre: producto.categorias.nombre},
+                img:producto.img,
+               }
+           }),
            status:200
        })
    })
 },
-admin:  (req,res) => {
-   db.administrador
-   .findAll()
-   .then(administrador => {
-       return res.status(200).json({
-           total: administrador.length,
-           data: administrador,
-           status:200
-       })
-   })
-},
+// admin:  (req,res) => {
+//    db.administrador
+//    .findAll()
+//    .then(administrador => {
+//        return res.status(200).json({
+//            total: administrador.length,
+//            data: administrador,
+//            status:200
+//        })
+//    })
+// },
 categorias:  (req,res) => {
     db.categorias
     .findAll()
     .then(categorias => {
         return res.status(200).json({
             total: categorias.length,
-            data: categtorias,
+            data: categorias.map(categoria=>{
+                return{
+                 id: categoria.id,
+                 nombre: categoria.nombre,
+                }
+            }),
             status:200
         })
     })
  },
- ventas: (req,res) => {
-    db.ventas
-    .create(req.body)
-    .then(ventas => {
-        return res.status(200).json({
-            data: ventas,
-            status:200,
-            created: 'ok'
-        })
-    })
- },
+//  ventas: (req,res) => {
+//     db.ventas
+//     .create(req.body)
+//     .then(ventas => {
+//         return res.status(200).json({
+//             data: ventas,
+//             status:200,
+//             created: 'ok'
+//         })
+//     })
+//  },
 }
 module.exports = controller;
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const apiController = require('../controllers/apiController');
 
 router.get('/', apiController.api);
-router.get('/admin', apiController.admin);
+
 router.get('/categorias', apiController.categorias);
-router.post('/ventas', apiController.ventas);
+
 
 
 module.exports = router;
